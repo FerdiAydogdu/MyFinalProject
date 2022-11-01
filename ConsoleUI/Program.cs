f5714ecccf5fc2bd44c4794a139834658a6d15e5
ferdi <aydogduferdi14@gmail.com>
2022-11-01 20:17:14 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    // SOLID
+    // Open Closed Principle : Programa yeni bir özellik eklediğinde hiçbir koda dokunmamalısın.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
